80f2cab9052cf45977edd169d76576ff1a92b4c2
id1emind <id1emind@outlook.com>
2018-01-21 00:28:49 +0000

    updating system.reactive nuget version

--- src/Purple.PurpleD/Program.cs
+++ src/Purple.PurpleD/Program.cs
@@ -27,54 +27,8 @@ namespace Purple.PurpleD
     {
         public static void Main(string[] args)
         {
-            //ParallelHash();
             MainAsync(args).Wait();
         }
-
-        public static void ParallelHash()
-        {
-            int nonce = 0;
-            int count = 0;
-
-            var watch = new Stopwatch();
-            watch.Start();
-
-            var target = new uint256("0xeb14e8a833fac6fe9a43b57b336789c46ffe93f2868452240720607b14387e11");
-
-            //Parallel.ForEach(Infinite(), (x, state) =>
-            //{
-            //    if (!state.ShouldExitCurrentIteration)
-            //    {
-            //        var hash = NBitcoin.Crypto.CryptoNight.Instance.Hash(Encoding.UTF8.GetBytes(""));
-            //        var match = hash == target;
-            //        Interlocked.Increment(ref nonce);
-            //        if (nonce == 1000)
-            //        {
-            //            state.Break();
-            //        }
-            //    }
-            //});
-
-            //while (nonce < 1000)
-            //{
-
-
-            //    ++nonce;
-            //    if (watch.ElapsedMilliseconds > 1000)
-            //    {
-            //        Console.WriteLine($"{nonce - last} H/s");
-            //        last = nonce;
-            //        watch.Restart();
-            //    }
-            //}
-
-            watch.Stop();
-
-            Console.WriteLine(watch.Elapsed.TotalSeconds);
-            Console.ReadLine();
-        }
-
-
 
         public static async Task MainAsync(string[] args)
         {
